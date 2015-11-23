ab480b418db235bba4fb1e7ef99722ee74486520
nepta <nepta23@gmail.com>
2015-11-23 21:44:57 +0100

    turtle class

---
+++ src/Turtle.java
@@ -0,0 +1,18 @@
+public class Turtle{
+	private double food;	//!< satiated percent
+	private double life;	//!< healt percent
+
+	public Turtle(){
+		this.food = 50;
+		this.life = 100;
+	}
+
+	public bool reproduce(Turtle wife){
+		if(this.food >= 85.0 && wife.food >= 85.0){
+			// reproduction state ("game engine" can create another turtle)
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
