82535b8400d724fbfeda00b424faaf6d93f71677
nepta <nepta23@gmail.com>
2015-11-23 22:58:21 +0100

    minimal server

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,38 @@
 import org.zeromq.ZMQ;
 
 public class Server{
+	private ZMQ.Context context;
+	private ZMQ.Socket socket;
 
+	public Server(int port){
+		this.context = ZMQ.context(1);
+		this.socket = context.socket(ZMQ.REP);
+		System.out.println("tcp://*:"+port);
+		this.socket.bind("tcp://*:"+port);
+	}
+
+	private String process(String message){
+		return "Ok";
+	}
+
+	public void run(){
+		String message;
+		do{
+			message = socket.recvStr();
+			System.out.println(message);
+			String response = process(message);
+			socket.send(response);
+		}while(!message.equals("quit"));
+	}
+
+	public static void main(String args[]){
+		int port;
+		if(args.length > 0){
+			port = new Integer(args[0]);
+		}else{
+			port = 5555;
+		}
+		Server server = new Server(port);
+      server.run();
+	}
 }
--- src/Turtle.java
+++ src/Turtle.java
@@ -7,7 +7,7 @@ public class Turtle{
 		this.life = 100;
 	}
 
-	public bool reproduce(Turtle wife){
+	public boolean reproduce(Turtle wife){
 		if(this.food >= 85.0 && wife.food >= 85.0){
 			// reproduction state ("game engine" can create another turtle)
 			return true;
