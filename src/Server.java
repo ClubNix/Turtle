694cf2776afd3ccde24e3532866df84549090946
fmutix <fmutix@gmail.com>
2015-11-24 18:33:27 +0100

    interpret received command string

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,59 @@
 import org.zeromq.ZMQ;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Server {
 	private ZMQ.Context context;
 	private ZMQ.Socket socket;
+	private Turtle turtle = new Turtle();
+
+	interface ProcessingMethod {
+		public String method();
+	}
+
+	private Map<String, ProcessingMethod> methodMap = new HashMap<String, ProcessingMethod>();
 
 	public Server(int port) {
 		this.context = ZMQ.context(1);
 		this.socket = context.socket(ZMQ.REP);
 		System.out.println("tcp://*:"+port);
 		this.socket.bind("tcp://*:"+port);
+		this.methodMap.put("up", new ProcessingMethod() {
+			public String method() {
+				turtle.up();
+				return "up";
+			}
+		});
+		this.methodMap.put("down", new ProcessingMethod() {
+			public String method() {
+				turtle.down();
+				return "down";
+			}
+		});
+		this.methodMap.put("left", new ProcessingMethod() {
+			public String method() {
+				turtle.left();
+				return "left";
+			}
+		});
+		this.methodMap.put("right", new ProcessingMethod() {
+			public String method() {
+				turtle.right();
+				return "right";
+			}
+		});
+		this.methodMap.put("position", new ProcessingMethod() {
+			public String method() {
+				return turtle.getStringPosition();
+			}
+		});
+		this.methodMap.put("quit", new ProcessingMethod() {
+			public String method() {
+				return "quit";
+			}
+		});
+
+
 	}
 
 	private String process(String message) {
@@ -20,7 +65,10 @@ public class Server {
 		do {
 			message = socket.recvStr();
 			System.out.println(message);
-			String response = process(message);
+			
+			String response = this.methodMap.get(message).method();
+			
+			//String response = message;
 			socket.send(response);
 		} while (!message.equals("quit"));
 	}
@@ -33,6 +81,6 @@ public class Server {
 			port = 5555;
 		}
 		Server server = new Server(port);
-      server.run();
+		server.run();
 	}
 }
--- src/Turtle.java
+++ src/Turtle.java
@@ -1,10 +1,14 @@
 public class Turtle {
-	private double food;	//!< satiated percent
-	private double life;	//!< healt percent
+	private double food;	//!< satiation percent
+	private double life;	//!< health percent
+	private int x;
+	private int y;
 
 	public Turtle() {
 		this.food = 50;
 		this.life = 100;
+		this.x = 0;
+		this.y = 0;
 	}
 
 	public boolean reproduce(Turtle wife) {
@@ -14,5 +18,33 @@ public class Turtle {
 		} else {
 			return false;
 		}
+	}
+
+	public void up() {
+		this.y++;
+	}
+
+	public void down() {
+		this.y--;
+	}
+
+	public void left() {
+		this.x--;
+	}
+
+	public void right() {
+		this.x++;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+	public int getY() {
+		return this.y;
+	}
+
+	public String getStringPosition() {
+		return "x:" + this.x + ",y:" + this.y;
 	}
 }
