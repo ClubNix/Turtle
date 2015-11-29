d9ba105c9d51c60771358c7a428fa9af441b32e4
nepta <nepta23@gmail.com>
2015-11-29 14:46:50 +0100

    player list

---
+++ src/PlayerList.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class PlayerList {
+   private HashMap<String,Player> playerList;
+
+	PlayerList() {
+		this.playerList = new HashMap<String, Player>()
+	}
+
+	public Player get(String id) {
+		return this.playerList.get(id)
+	}
+
+	public void add(String id, Player player) {
+		this.playerList.put(id, player);
+	}
+}
